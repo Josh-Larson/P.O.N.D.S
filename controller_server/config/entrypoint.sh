16f043051b3f753e8ebc393f0bf86a2ef537d495
Connor Hamlet <cphamlet@live.unc.edu>
2018-08-11 17:24:28 -0400

    removed nothingburger lines

--- controller_server/config/entrypoint.sh
+++ controller_server/config/entrypoint.sh
@@ -1,8 +1,5 @@
-#apt install python3-flask python3-flask-login python3-websockets python3-gunicorn -y
 pip3 install -r /usr/src/app/requirements.txt
-#python3 /usr/src/app/mainserver.py
-#gunicorn3 -w 3 --bind 0.0.0.0:5000 flaskApp:wsgi
-python3 /usr/src/app/flaskApp.py &
 
+#Starts flask and websocket servers
+python3 /usr/src/app/flaskApp.py &
 python3 /usr/src/app/socketServer.py 
-#python3 /usr/src/app/flaskApp.py 
