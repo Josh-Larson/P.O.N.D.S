675e37a02a90c82767b0a62ec0fe8cb47c116a07
Connor Hamlet <cphamlet@live.unc.edu>
2018-08-11 15:54:43 -0400

    added start script for pondPi

--- pond_client/startContainer.sh
+++ pond_client/startContainer.sh
@@ -4,5 +4,25 @@
 #for both RPI clients to be:
 # "pondPiEast" and "pondPiWest".
 
-HOSTNAME=$(hostname)
-docker run -h $HOSTNAME --name pondPi -d  --device /dev/ttyAMA0:/dev/ttyAMA0 --device /dev/mem:/dev/mem --device /dev/gpiomem:/dev/gpiomem --privileged rpi
+echo "Starting container script...\n"
+
+echo "What is the ip address of the central server you are connecting to?"
+
+read CENTRAL_SERVER_IP  
+
+echo "Enter the number to select your choice: "
+echo " 1. Set this pi up as pondPiEast. "
+echo " 2. Set this pi up as pondPiWest. "
+read HOSTNAME
+
+if [ $HOSTNAME -eq 1 ]
+then HOSTNAME=pondPiEast
+fi
+
+if [ $HOSTNAME -eq 2 ]
+then HOSTNAME=pondPiWest
+fi
+
+echo $HOSTNAME
+
+docker run -h $HOSTNAME -e "CENTRAL_SERVER_IP=$CENTRAL_SERVER_IP" --name pondPi -d  --device /dev/ttyAMA0:/dev/ttyAMA0 --device /dev/mem:/dev/mem --device /dev/gpiomem:/dev/gpiomem --privileged rpi
